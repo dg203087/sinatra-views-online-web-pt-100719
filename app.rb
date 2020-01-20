8731b9f656f07576b6c2451fd5039de7a9a4758e
Deanna Kelley <deanna.georges@gmail.com>
2020-01-20 15:53:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,12 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-	get '/' do
-		erb :index 
-	end
-	
-	get "/info" do
-	  erb :info
+	get "/index" do
+	  erb :index
 	end 
 	
 end
